fcd52656fd92e3f0a8f50e7e8f7e5783dba78bc5
pasha55557 <al.paha@yandex.ru>
2020-01-24 17:05:58 +0300

    norm pro_w

--- src/pro_w.c
+++ src/pro_w.c
@@ -6,31 +6,25 @@
 /*   By: rsticks <rsticks@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/24 16:37:34 by rsticks           #+#    #+#             */
-/*   Updated: 2020/01/24 16:38:21 by rsticks          ###   ########.fr       */
+/*   Updated: 2020/01/24 17:04:54 by rsticks          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 
-void		process_width(t_printf *prnt, char *c)
+static void					process_width1(t_printf *prnt, char *c, int *count)
 {
-	char	*ptr;
-	int		count;
-	int		acc;
+	int acc;
 
-	ptr = c;
-	count = 0;
-	count = ft_strlen(c);
-	acc = count;
-	if (FLAG_NULL == (prnt->flags & FLAG_NULL) && (prnt->accuracy >= 0))
-		prnt->flags -= FLAG_NULL;
+	acc = *count;
 	if (c[0] == '-')
 		acc--;
-	if ((FLAG_PLUS == (prnt->flags & FLAG_PLUS) || FLAG_SPACE == (prnt->flags & FLAG_SPACE)) && (c[0] != '-'))
-		count++;
+	if ((FLAG_PLUS == (prnt->flags & FLAG_PLUS) ||
+	FLAG_SPACE == (prnt->flags & FLAG_SPACE)) && (c[0] != '-'))
+		(*count)++;
 	if (prnt->accuracy == 0 && *c == '0')
 	{
-		count--;
+		(*count)--;
 		if (prnt->width == 0)
 			prnt->accuracy = -4;
 		else
@@ -40,14 +34,18 @@ void		process_width(t_printf *prnt, char *c)
 	{
 		prnt->accuracy -= acc;
 		if (prnt->accuracy > 0)
-			count += prnt->accuracy;
+			(*count) += prnt->accuracy;
 		else
 			prnt->accuracy = -2;
 	}
-	
-	if (FLAG_MINUS != (prnt->flags & FLAG_MINUS) && (FLAG_NULL != (prnt->flags & FLAG_NULL)))
+}
+
+static void					process_width2(t_printf *prnt, char *c, int *count)
+{
+	if (FLAG_MINUS != (prnt->flags & FLAG_MINUS) &&
+	(FLAG_NULL != (prnt->flags & FLAG_NULL)))
 	{
-		prnt->width -= count;
+		prnt->width -= (*count);
 		while (prnt->width > 0)
 		{
 			putchar_and_count(prnt, ' ');
@@ -61,20 +59,25 @@ void		process_width(t_printf *prnt, char *c)
 		if (c[0] != '-' || c[0] == 0)
 			putchar_and_count(prnt, '+');
 	}
-	if (FLAG_SPACE == (prnt->flags & FLAG_SPACE) && (FLAG_PLUS != (prnt->flags & FLAG_PLUS)))
+	if (FLAG_SPACE == (prnt->flags & FLAG_SPACE) &&
+	(FLAG_PLUS != (prnt->flags & FLAG_PLUS)))
 	{
 		if (c[0] != '-' || c[0] == 0)
 			putchar_and_count(prnt, ' ');
 	}
+}
+
+static void					process_width3(t_printf *prnt, char *c, int *count)
+{
 	while (prnt->accuracy > 0)
 	{
 		putchar_and_count(prnt, '0');
 		prnt->accuracy--;
 	}
-		
-	if (FLAG_NULL == (prnt->flags & FLAG_NULL) && FLAG_MINUS != (prnt->flags & FLAG_MINUS))
+	if (FLAG_NULL == (prnt->flags & FLAG_NULL) &&
+	FLAG_MINUS != (prnt->flags & FLAG_MINUS))
 	{
-		prnt->width -= count;
+		prnt->width -= (*count);
 		while (prnt->width > 0)
 		{
 			putchar_and_count(prnt, '0');
@@ -89,6 +92,22 @@ void		process_width(t_printf *prnt, char *c)
 			putchar_and_count(prnt, *c);
 		c++;
 	}
+}
+
+void						process_width(t_printf *prnt, char *c)
+{
+	char	*ptr;
+	int		count;
+
+	ptr = c;
+	count = 0;
+	count = ft_strlen(c);
+	
+	if (FLAG_NULL == (prnt->flags & FLAG_NULL) && (prnt->accuracy >= 0))
+		prnt->flags -= FLAG_NULL;
+	process_width1(prnt, c, &count);
+	process_width2(prnt, c, &count);
+	process_width3(prnt, c, &count);
 	if (FLAG_MINUS == (prnt->flags & FLAG_MINUS))
 	{
 		prnt->width -= count;

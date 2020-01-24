bce2f33d744d1f5648c49abe5e8e2edc0168f636
pasha55557 <al.paha@yandex.ru>
2020-01-24 15:48:20 +0300

    norm integer.c

--- src/integer.c
+++ src/integer.c
@@ -6,33 +6,54 @@
 /*   By: rsticks <rsticks@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/01 16:46:01 by rsticks           #+#    #+#             */
-/*   Updated: 2019/12/10 15:28:03 by rsticks          ###   ########.fr       */
+/*   Updated: 2020/01/24 15:47:52 by rsticks          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 
+static char			*mod_l(t_printf *prnt)
+{
+	char			*c;
+	long long int	ln;
+
+	ln = va_arg(prnt->arg, long int);
+	c = ft_long_itoa((long long int)ln);
+	return (c);
+}
+
+static char			*mod_ll(t_printf *prnt)
+{
+	char			*c;
+	long long int	ln;
+
+	ln = va_arg(prnt->arg, long long int);
+	c = ft_long_itoa(ln);
+	return (c);
+}
+
+static char			*mod_h(t_printf *prnt)
+{
+	char			*c;
+	long long int	ln;
+
+	ln = (short int)va_arg(prnt->arg, int);
+	c = ft_long_itoa((long long int)ln);
+	return (c);
+}
+
 char				*modificator(t_printf *prnt)
 {
 	char			*c;
-	long long int 	ln;
+	long long int	ln;
 
 	ln = 0;
 	if (MOD_L == (prnt->flags & MOD_L))
-	{
-		ln = va_arg(prnt->arg, long int);
-		c = ft_long_itoa((long long int)ln);
-	}
+		c = mod_l(prnt);
 	else if (MOD_LL == (prnt->flags & MOD_LL))
-	{
-		ln = va_arg(prnt->arg, long long int);
-		c = ft_long_itoa(ln);
-	}
+		c = mod_ll(prnt);
 	else if (MOD_H == (prnt->flags & MOD_H))
-	{
-		ln = (short int)va_arg(prnt->arg, int);
-		c = ft_long_itoa((long long int)ln);
-	}
+		c = mod_h(prnt);
 	else if (MOD_HH == (prnt->flags & MOD_HH))
 	{
 		ln = (char)va_arg(prnt->arg, int);
@@ -46,11 +67,10 @@ char				*modificator(t_printf *prnt)
 	return (c);
 }
 
-void		ft_intger(t_printf *prnt)
+void				ft_intger(t_printf *prnt)
 {
-	char	*c;
+	char			*c;
 
 	c = modificator(prnt);
 	process_width(prnt, c);
 }
-
